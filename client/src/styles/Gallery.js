50a7caddaadab2c7a1d1b1816fcb4b776852f259
Romeo-Richardson <111822421+Romeo-Richardson@users.noreply.github.com>
2023-01-17 06:03:11 -0800

    Update Gallery.js

--- client/src/styles/Gallery.js
+++ client/src/styles/Gallery.js
@@ -5,9 +5,8 @@ width: 1720px;
 position: relative;
 top: 270px;
 display: flexbox;
-flex-direction: column;
 flex-wrap: wrap;
-justify-content: space-between;
+flex-direction: column;
 
 @media screen and (max-width: 1750px) {
     width: 80vw;
